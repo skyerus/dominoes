609dd0ef2fbb215d97abccec5a96b4749f3cca10
Skye Gill <gill.skye95@gmail.com>
2020-06-22 21:25:20 +0100

    Player tiles front

--- web/src/js/components/domino.js
+++ web/src/js/components/domino.js
@@ -6,6 +6,7 @@ const useStyles = makeStyles(theme => ({
     border: "1px solid #fff",
     borderRadius: "2px",
     display: "flex",
+    cursor: "default",
   },
   horizontalContainer: {
     width: "160px",
@@ -14,6 +15,15 @@ const useStyles = makeStyles(theme => ({
   verticalContainer: {
     width: "80px",
     height: "160px",
+    flexDirection: "column",
+  },
+  smallHorizontalContainer: {
+    width: "120px",
+    height: "60px",
+  },
+  smallVerticalContainer: {
+    width: "60px",
+    height: "120px",
     flexDirection: "column",
   },
   numberContainer: {
@@ -33,8 +43,22 @@ const useStyles = makeStyles(theme => ({
 
 export default function Domino(props) {
   const classes = useStyles()
+  const dimensionStyle = () => {
+    if (props.vertical) {
+      if (props.small) {
+        return classes.smallVerticalContainer
+      }
+      return classes.verticalContainer
+    } else {
+      if (props.small) {
+        return classes.smallHorizontalContainer
+      }
+      return classes.horizontalContainer
+    }
+  }
+
   return (
-    <div className={`${classes.container} ${props.vertical ? classes.verticalContainer : classes.horizontalContainer}`}>
+    <div className={`${classes.container} ${dimensionStyle()}`}>
       <div className={`${classes.numberContainer} ${props.vertical ? classes.verticalNumberContainer : classes.horizontalNumberContainer}`}>
         <div>{props.left}</div>
       </div>
--- web/src/js/components/game.js
+++ web/src/js/components/game.js
@@ -1,9 +1,28 @@
 import React, {useEffect} from "react"
+import { makeStyles } from '@material-ui/core/styles'
 import Domino from './domino'
 
 import coreApi from '../api/core'
 
+const useStyles = makeStyles(theme => ({
+  playerTilesContainer: {
+    backgroundColor: "#666666",
+    position: "absolute", 
+    bottom: "0", 
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    paddingTop: "20px",
+    paddingBottom: "20px",
+  },
+  playerDominoContainer: {
+    marginLeft: "7px",
+    marginRight: "7px",
+  }
+}))
+
 export default function Game(props) {
+  const classes = useStyles()
   const [gameState, setGameState] = React.useState(null)
 
   useEffect(() => {
@@ -28,10 +47,20 @@ export default function Game(props) {
     })
   }
 
-  return (
+  return gameState && (
     <div>
-      <div style={{position: "absolute", bottom: "0", marginLeft: "20px", marginRight: "20px"}}>
-        <Domino left={0} right={1} vertical={true}/>
+      <div className={classes.playerTilesContainer}>
+        {
+          gameState.my_tiles.map((t, i) =>
+            <div className={classes.playerDominoContainer}>
+              <Domino key={i}
+                      left={t.left} 
+                      right={t.right} 
+                      vertical={true}
+              />
+            </div>
+          )
+        }
       </div>
     </div>
   )
